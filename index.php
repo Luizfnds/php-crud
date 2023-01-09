6c5435accb0be9c4751c62fc635ba6c2de329372
Luizfnds <luizeduardofnds@gmail.com>
2023-01-09 20:24:56 -0300

    Delete

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
     $pdo = new PDO("mysql:host=localhost;dbname=crud", "root" ,"0000");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+
+    if(isset($_GET["delete"])) {
+        $id = (int)$_GET["delete"];
+        $pdo->exec("DELETE FROM users WHERE idUser=$id");
+        echo "deletado com sucesso!";
+    }
+
     if(isset($_POST["name"])) {
         $sql = $pdo->prepare("INSERT INTO users VALUES (null, ?, ?)");
         $sql->execute(array($_POST["name"], $_POST["email"]));
@@ -24,8 +31,7 @@
     $fetchUsers = $sql->fetchAll();
 
     foreach($fetchUsers as $key => $value) {
-        echo $value;
-        echo $value["nameUser"]." | ".$value["emailUser"];
+        echo "<a href='?delete=".$value['idUser']."'>(X)</a>".$value["nameUser"]." | ".$value["emailUser"];
         echo "<hr>";
     }
 ?>
