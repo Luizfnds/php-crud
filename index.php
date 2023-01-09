8446e4b0eba1d47cdf425c89f2b4b2180a7eccda
Luizfnds <luizeduardofnds@gmail.com>
2023-01-09 18:13:10 -0300

    Exibindo erros

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
     $pdo = new PDO("mysql:host=localhost;dbname=crud", "root" ,"0000");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if(isset($_POST["name"])) {
         $sql = $pdo->prepare("INSERT INTO users VALUES (null, ?, ?)");
